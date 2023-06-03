845be527801c578bc4a903b5796680815de3ddda
Edgar Rojas <redgarm@gmail.com>
2023-06-03 13:02:55 -0600

    Actualización Sesión Viernes 2 de Junio 2023

--- routes/Route.js
+++ routes/Route.js
@@ -1,23 +1,45 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import HomeScreen from "../views/HomeScreen";
 import AlumnoScreen from "../views/AlumnoScreen";
 import DocenteScreen from "../views/DocenteScreen";
 import MateriaScreen from "../views/MateriaScreen";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function Route() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Alumno" component={AlumnoScreen} />
-        <Stack.Screen name="Docente" component={DocenteScreen} />
-        <Stack.Screen name="Materia" component={MateriaScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home-outline" : "home";
+            } else if (route.name === "Alumno") {
+              iconName = focused ? "people-outline" : "people";
+            } else if (route.name === "Docente") {
+              iconName = focused ? "body-outline" : "body";
+            } else if (route.name === "Materia") {
+              iconName = focused ? "book-outline" : "book";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Alumno" component={AlumnoScreen} />
+        <Tab.Screen name="Docente" component={DocenteScreen} />
+        <Tab.Screen name="Materia" component={MateriaScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- views/HomeScreen.js
+++ views/HomeScreen.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
+import { View, Text } from "react-native";
 
 export default function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Pantalla principal</Text>
-      <Button title="Alumnos" onPress={() => navigation.navigate("Alumno")} />
-      <Button title="Docentes" onPress={() => navigation.navigate("Docente")} />
-      <Button title="Materias" onPress={() => navigation.navigate("Materia")} />
+      <Text>HOME</Text>
     </View>
   );
 }
